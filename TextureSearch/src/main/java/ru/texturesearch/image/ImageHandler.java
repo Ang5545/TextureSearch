cc3b43af8edc69a59518bcd92f5512a592d20d57
Ang5545 <Ange55455@gmail.com>
2016-10-25 01:46:51 +0300

    ds

--- TextureSearch/src/main/java/ru/texturesearch/image/ImageHandler.java
+++ TextureSearch/src/main/java/ru/texturesearch/image/ImageHandler.java
@@ -58,10 +58,41 @@ public class ImageHandler {
 	
 	private int getLBPval(int val, int val2) {
 		if ((val2 - val) >= 0) {
-			return 0;
-		} else {
 			return 1;
+		} else {
+			return 0;
 		}
+	}
+	
+	
+	public boolean isUniformLbp(String bits) {
+		char c = 0;
+		int transitionCount = 0;
+		for (int i = 0; i < bits.length(); i++) {
+			if (c != bits.charAt(i)) {
+				transitionCount++;
+			}
+			c = bits.charAt(i);
+		}
+		if (transitionCount > 3) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	
+	public int getLbp(String val) {
+		int result = 0;
+		if (val.charAt(0) == '1') result += 1;
+		if (val.charAt(1) == '1') result += 2;
+		if (val.charAt(2) == '1') result += 4;
+		if (val.charAt(3) == '1') result += 8;
+		if (val.charAt(4) == '1') result += 16;
+		if (val.charAt(5) == '1') result += 32;
+		if (val.charAt(6) == '1') result += 64;
+		if (val.charAt(7) == '1') result += 128;
+		return result;
 	}
 	
 	private List<Integer> calculateLBP_1(IplImage img, IplImage result) {
@@ -69,41 +100,73 @@ public class ImageHandler {
 		
 		ByteBuffer bb_img = img.createBuffer();
 		ByteBuffer bb_result = result.createBuffer();
-		
-		
+
 		int ws = img.widthStep();
 		int ch = img.nChannels();
 
+		
+		int all = 0;
+		int isUniform = 0;
+		
 		for(int y = 1; y < img.height() - 1; y++) {
 		    for(int x = 1; x < img.width() - 1; x++) {
 		        // 7   0   1
 		    	// 6   val 2
 		    	// 5   4   3
 		    	
+//		    	int index = y * ws + x * ch;  
+//		        int val = 	getValFromByteBuffer(x, y, bb_img, ws, ch);
+//		        int val_0 = getLBPval(val, getValFromByteBuffer(x, y+1, bb_img, ws, ch))	== 0 ? 0 : 1;
+//		        int val_1 = getLBPval(val, getValFromByteBuffer(x+1, y+1, bb_img, ws, ch))	== 0 ? 0 : 2;
+//		        int val_2 = getLBPval(val, getValFromByteBuffer(x+1, y, bb_img, ws, ch))	== 0 ? 0 : 4;
+//		        int val_3 = getLBPval(val, getValFromByteBuffer(x+1, y-1, bb_img, ws, ch))	== 0 ? 0 : 8;
+//		        int val_4 = getLBPval(val, getValFromByteBuffer(x, y-1, bb_img, ws, ch))	== 0 ? 0 : 16;
+//		        int val_5 = getLBPval(val, getValFromByteBuffer(x-1, y-1, bb_img, ws, ch))	== 0 ? 0 : 32;
+//		        int val_6 = getLBPval(val, getValFromByteBuffer(x-1, y, bb_img, ws, ch))	== 0 ? 0 : 64;
+//		        int val_7 = getLBPval(val, getValFromByteBuffer(x- 1, y+1, bb_img, ws, ch))	== 0 ? 0 : 128;
+//
+//		        int resVal = val_0 + val_1 + val_2 + val_3 + val_4 
+//		        		+ val_5 + val_6 + val_7;
+//		        
+//		        lbpList.add(resVal);
+//		        bb_result.put(index, (byte) resVal);	        
+		    	
+// --------------------------------------------------------------------------------------------------------
+		    	
 		    	int index = y * ws + x * ch;  
 		        int val = 	getValFromByteBuffer(x, y, bb_img, ws, ch);
-		        int val_0 = getLBPval(val, getValFromByteBuffer(x, y+1, bb_img, ws, ch))	== 0 ? 0 : 1;
-		        int val_1 = getLBPval(val, getValFromByteBuffer(x+1, y+1, bb_img, ws, ch))	== 0 ? 0 : 2;
-		        int val_2 = getLBPval(val, getValFromByteBuffer(x+1, y, bb_img, ws, ch))	== 0 ? 0 : 4;
-		        int val_3 = getLBPval(val, getValFromByteBuffer(x+1, y-1, bb_img, ws, ch))	== 0 ? 0 : 8;
-		        int val_4 = getLBPval(val, getValFromByteBuffer(x, y-1, bb_img, ws, ch))	== 0 ? 0 : 16;
-		        int val_5 = getLBPval(val, getValFromByteBuffer(x-1, y-1, bb_img, ws, ch))	== 0 ? 0 : 32;
-		        int val_6 = getLBPval(val, getValFromByteBuffer(x-1, y, bb_img, ws, ch))	== 0 ? 0 : 64;
-		        int val_7 = getLBPval(val, getValFromByteBuffer(x- 1, y+1, bb_img, ws, ch))	== 0 ? 0 : 128;
+		        int val_0 = getLBPval(val, getValFromByteBuffer(x, y+1, bb_img, ws, ch));
+		        int val_1 = getLBPval(val, getValFromByteBuffer(x+1, y+1, bb_img, ws, ch));
+		        int val_2 = getLBPval(val, getValFromByteBuffer(x+1, y, bb_img, ws, ch));
+		        int val_3 = getLBPval(val, getValFromByteBuffer(x+1, y-1, bb_img, ws, ch));
+		        int val_4 = getLBPval(val, getValFromByteBuffer(x, y-1, bb_img, ws, ch));
+		        int val_5 = getLBPval(val, getValFromByteBuffer(x-1, y-1, bb_img, ws, ch));;
+		        int val_6 = getLBPval(val, getValFromByteBuffer(x-1, y, bb_img, ws, ch));
+		        int val_7 = getLBPval(val, getValFromByteBuffer(x- 1, y+1, bb_img, ws, ch));
+		    	
+		        String bits = String.valueOf(val_0)+ String.valueOf(val_1) 
+		        		+ String.valueOf(val_2) + String.valueOf(val_3) 
+		        		+ String.valueOf(val_4) + String.valueOf(val_5) 
+		        		+ String.valueOf(val_6) + String.valueOf(val_7);
+
+		        if (isUniformLbp(bits)) {
+		        	int resultVal = getLbp(bits);
+		        	bb_result.put(index, (byte) resultVal);	   
+		        	lbpList.add(resultVal);
+		        }
+		       // byte b = (byte) Integer.parseInt(bits, 2);
 		        
-//		        String bits = String.valueOf(val_0)+ String.valueOf(val_1) 
-//		        		+ String.valueOf(val_2) + String.valueOf(val_3) 
-//		        		+ String.valueOf(val_4) + String.valueOf(val_5) 
-//		        		+ String.valueOf(val_6) + String.valueOf(val_7);
-//		        byte b = (byte) Integer.parseInt(bits, 2);
 		        
-		        int resVal = val_0 + val_1 + val_2 + val_3 + val_4 
-		        		+ val_5 + val_6 + val_7;
 		        
-		        lbpList.add(resVal);
-		        bb_result.put(index, (byte) resVal);	        
+//		        System.out.println("bits = " + bits);
+//		        System.out.println("b = " + b );
+//		        
+		        
+		        
 		    }
 		}
+		System.out.println("all = " + all);
+		System.out.println("isUniform = " + isUniform);
 		return lbpList;
 	}
 	
