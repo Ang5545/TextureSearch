006f5be6895159807651768a92f74b65d079d092
Ang5545 <Ange55455@gmail.com>
2016-10-28 02:09:28 +0300

    test serm algoritm

---
+++ TextureSearch/src/main/java/ru/texturesearch/test/App.java
@@ -0,0 +1,67 @@
+package ru.texturesearch.test;
+
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import ru.texturesearch.gui.ImagePanel;
+import ru.texturesearch.image_utils.ImageHelper;
+
+
+public class App {
+
+	private static final String WINDOW_NAME = "Main Window";
+	private static final int HEIGHT = 500;
+	private static final int WEIGHT = 300;
+	
+	private ImagePanel imgPan;
+	private JFrame jfrm;
+	private Srm srm;
+			
+	private App(){
+		this.jfrm = new JFrame(WINDOW_NAME);
+		this.jfrm.getContentPane().setLayout(new FlowLayout());
+		this.jfrm.setSize(HEIGHT, WEIGHT);
+		this.jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.imgPan = new ImagePanel(WEIGHT -20, WEIGHT - 20, "Result");
+		this.jfrm.add(imgPan);
+		
+		this.srm = new Srm();
+		
+		JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 100, 25);
+		slider.setMajorTickSpacing(10);
+		slider.setMinorTickSpacing(1);
+		slider.setPaintTicks(true);
+		slider.setPaintLabels(true);
+		slider.addChangeListener(new ChangeListener() {
+		    public void stateChanged(ChangeEvent e) {
+		    	JSlider slider = (JSlider) e.getSource();
+		    	float val = slider.getValue();
+		    	srm.run(val);
+				setImage(ImageHelper.getBufferedImage(srm.getResult()));
+		    }
+		});
+		this.jfrm.add(slider);
+	}
+	
+	public void show() {
+		this.jfrm.setVisible(true);
+	}
+	
+	public void setImage(BufferedImage img) {
+		this.imgPan.setImage(img);
+	}
+	
+	
+	public static void main(String[] args) {
+		App app = new App();
+		app.show();
+	}
+	
+	
+}
---
+++ TextureSearch/src/main/java/ru/texturesearch/test/Srm.java
@@ -0,0 +1,200 @@
+package ru.texturesearch.test;
+
+import static org.bytedeco.javacpp.opencv_core.IPL_DEPTH_8U;
+import static org.bytedeco.javacpp.opencv_core.cvCreateImage;
+import static org.bytedeco.javacpp.opencv_imgcodecs.cvLoadImage;
+import static org.bytedeco.javacpp.opencv_imgproc.CV_BGR2GRAY;
+import static org.bytedeco.javacpp.opencv_imgproc.cvCvtColor;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+import org.bytedeco.javacpp.opencv_core.CvSize;
+import org.bytedeco.javacpp.opencv_core.IplImage;
+
+
+public class Srm {
+
+	int[] nextNeighbor;
+	int[] neighborBucket;
+	 
+	float[] average;
+	int[] count;
+	int[] regionIndex; // if < 0, it is -1 - actual_regionIndex
+	
+	float g = 256; // number of different intensity values
+	//float Q = 25; //25; // complexity of the assumed distributions
+	
+	protected float delta;
+	
+	protected float factor, logDelta;
+	
+	private IplImage gray;
+	private ByteBuffer bb;
+	
+	private IplImage result;
+	private ByteBuffer bb_result;
+
+	//String path = "/Users/fedormurashko/Pictures/Images/xHFxHFSbQyk.jpg";
+	//String path = "/Users/fedormurashko/Pictures/Images/Mems/l_2e6ec111.jpg";
+	//String path = "/Users/fedormurashko/Pictures/Images/Mems/1372415283_zve37.jpg";
+	//String path = "/Users/fedormurashko/Desktop/IMG_1879.JPG";
+	String path = "/Users/fedormurashko/Desktop/maxresdefault 2.jpg";
+	
+	
+	public Srm () {}
+		
+	
+	public void run(float Q) {
+		
+		IplImage img = new IplImage();
+		img = cvLoadImage(path, 3);
+		int w = img.width();
+		int h = img.height();
+
+		gray = cvCreateImage(new CvSize(w, h), IPL_DEPTH_8U, 1);
+		cvCvtColor(img, gray, CV_BGR2GRAY);
+
+		result = cvCreateImage(new CvSize(w, h), IPL_DEPTH_8U, 1);
+		bb_result = result.createBuffer();
+		
+		
+		delta = 1f / (6 * w * h);
+		factor = g * g / 2 / Q;
+		logDelta = 2f * (float) Math.log(6 * w * h);
+		
+
+		bb = gray.createBuffer();
+		
+		initializeRegions2D(bb, w, h);
+		initializeNeighbors2D(bb, w, h);
+		mergeAllNeighbors2D(w);
+		
+		boolean showAverages = true;
+		
+		if (showAverages) {
+			for (int i = 0; i < average.length; i++) {
+				//average[i] = average[getRegionIndex(i)];
+				bb.put(i, (byte) getRegionIndex(i));
+			}
+			
+			//ip = new FloatProcessor(w, h, average, null);
+		}
+		
+		//System.out.println("done");
+	}
+
+	void initializeRegions2D(ByteBuffer bb, int w, int h) {
+		average = new float[w * h];
+		count = new int[w * h];
+		regionIndex = new int[w * h];
+
+		for (int i = 0; i < average.length; i++) {
+			average[i] = bb.get(i) & 0xff;
+			count[i] = 1;
+			regionIndex[i] = i;
+		}
+	}
+	
+	void initializeNeighbors2D(ByteBuffer bb, int w, int h) {
+		nextNeighbor = new int[2 * w * h];
+
+		// bucket sort
+		neighborBucket = new int[256];
+		Arrays.fill(neighborBucket, -1);
+
+		for (int j = h - 1; j >= 0; j--)
+			for (int i = w - 1; i >= 0; i--) {
+				int index = i + w * j;
+				int neighborIndex = 2 * index;
+
+				// vertical
+				if (j < h - 1)
+					addNeighborPair(neighborIndex + 1, bb, index, index + w);
+
+				// horizontal
+				if (i < w - 1)
+					addNeighborPair(neighborIndex, bb, index, index + 1);
+			}
+	}
+	
+	protected void addNeighborPair(int neighborIndex, ByteBuffer bb, int i1, int i2) {
+		int difference = Math.abs((bb.get(i1) & 0xff) - (bb.get(i2) & 0xff));
+		nextNeighbor[neighborIndex] = neighborBucket[difference];
+		neighborBucket[difference] = neighborIndex;
+	}
+	
+	
+	void mergeAllNeighbors2D(int w) {
+		for (int i = 0; i < neighborBucket.length; i++) {
+			int neighborIndex = neighborBucket[i];
+			while (neighborIndex >= 0) {
+				int i1 = neighborIndex / 2;
+				int i2 = i1	+ (0 == (neighborIndex & 1) ? 1 : w);
+
+				i1 = getRegionIndex(i1);
+				i2 = getRegionIndex(i2);
+
+				if (predicate(i1, i2)) {
+					mergeRegions(i1, i2);
+				}
+				neighborIndex = nextNeighbor[neighborIndex];
+			}
+		}
+	}
+	
+	int getRegionIndex(int i) {
+		i = regionIndex[i];
+		while (i < 0)
+			i = regionIndex[-1 - i];
+		return i;
+	}
+	
+	// should regions i1 and i2 be merged?
+	boolean predicate(int i1, int i2) {
+		float difference = average[i1] - average[i2];
+		/*
+		 * This would be the non-relaxed predicate mentioned in the
+		 * paper.
+		 *
+		 * return difference * difference <
+			factor * (1f / count[i1] + 1f / count[i2]);
+		 *
+		 */
+		float log1 = (float)Math.log(1 + count[i1])
+			* (g < count[i1] ? g : count[i1]);
+		float log2 = (float)Math.log(1 + count[i2])
+			* (g < count[i2] ? g : count[i2]);
+		return difference * difference <
+			.1f * factor * ((log1 + logDelta) / count[i1]
+				+ ((log2 + logDelta) / count[i2]));
+	}
+	
+	void mergeRegions(int i1, int i2) {
+		if (i1 == i2)
+			return;
+		int mergedCount = count[i1] + count[i2];
+		float mergedAverage = (average[i1] * count[i1] + average[i2] * count[i2]) / mergedCount;
+
+		// merge larger index into smaller index
+		if (i1 > i2) {
+			average[i2] = mergedAverage;
+			count[i2] = mergedCount;
+			regionIndex[i1] = -1 - i2;
+		}
+		else {
+			average[i1] = mergedAverage;
+			count[i1] = mergedCount;
+			regionIndex[i2] = -1 - i1;
+		}
+	}
+	
+	public IplImage getResult() {
+		return gray;
+	}
+	
+	public static void main(String[] args) {
+		Srm srm = new Srm();
+	}
+
+}
